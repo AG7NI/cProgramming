9520751b98185af18f1eead76f4a4735d5e22104
Coursera Learner <nobody@nowhere.nul>
2018-10-11 20:06:32 +0000

    Did assignment 0

---
+++ 25_break_encr/break_encr.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+int breakEncry (FILE *f){
+  int c;
+  int alpha[26]= {0};
+  while ((c = fgetc(f)) != EOF) {        
+    if (isalpha(c)) {            
+      c = tolower(c);
+      c -= 'a';
+      // printf("%d\n",c);
+      alpha[c]+= 1;
+    }   
+  }
+  int maxFreq = 0;
+  int e_index = 27;
+  for (int i = 0; i < 26; i++){
+    if (alpha[i] > maxFreq){
+      maxFreq=alpha[i];
+      e_index = i;
+    }
+    //printf("%d ", alpha[i]);
+  }
+  return e_index;
+}
+int main (int argc, char **argv){
+  if (argc != 2){
+    fprintf(stderr,"Usage: encrypt key inputFileName\n");    
+    return EXIT_FAILURE;
+  }
+  FILE *f = fopen(argv[1], "r");
+  if (f == NULL) {    
+    perror("Could not open file");    
+    return EXIT_FAILURE;  
+  }  
+  int e_pos = breakEncry(f);
+  if (e_pos == 27){
+    perror("The txt file does not contain letters");
+    return EXIT_FAILURE;
+  }
+  printf("%d\n", e_pos);
+  if (fclose(f) != 0) {    
+    perror("Failed to close the input file!");    
+    return EXIT_FAILURE;  
+  }  
+  return EXIT_SUCCESS;
+}
+  
